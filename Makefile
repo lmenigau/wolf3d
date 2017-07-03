a2cbb455babf1d8e145c146cf6ee7f8366128140
Louis MENIGAULT <lmenigau@e3r1p12.42.fr>
2017-07-03 04:40:50 +0200

    afd

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@
 NAME	= wolf3d
 SRC		= wolf3d.c map.c plotline.c
 OBJ		= $(SRC:.c=.o)
-CFLAGS	= -Wall -Wextra -Werror -Ilibft/ -I../minilibx_macos/ -g 
+CFLAGS	= -Wall -Wextra -Werror -Ilibft/ -I../minilibx_macos/ -g -fsanitize=address
 LFLAGS	= -lmlx -framework Appkit -framework OpenGl
 LDFLAGS	= -Llibft/ -L../minilibx_macos/ 
 LIBDIR	= libft/
--- wolf3d.c
+++ wolf3d.c
@@ -9,27 +9,32 @@
 /*   Updated: 2017/05/24 20:33:12 by lmenigau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "wolf3d.h"
 #include "fdf.h"
 
 #include <stdio.h> //
 
+int		project(double x)
+{
+		return (x * (1080 / 50) + 10);
+}
+
 t_vec		dda(t_world *world, t_vec ray, t_vec delta)
 {
-	char	(*map)[(int)world->size.x];
+	char	(*map)[50];
 
-	map = (char (*)[])world->map;
+	map = (char (*)[50])world->map;
 	map[15][27] = 'X';
 	map[25][30] = 'X';
 	while (ray.x >= 0 && ray.x < world->size.x - 1
 			&& ray.y >= 0 && ray.y < world->size.y  -1)
 	{
+		world->debug[project(ray.y)][project(ray.x)] = 0xF0FF33;
 		if(map[(int)(ray.y)][(int)(ray.x)] != ' ')
 		{
-			printf("ray: %d %d double:", (int)ray.x, (int)ray.y);
-			printf("ray: %f %f\n", ray.x, ray.y);
-			printf("delta: %f %f\n", delta.x, delta.y);
+			/* printf("ray: %d %d double:", (int)ray.x, (int)ray.y); */
+			/* printf("ray: %f %f\n", ray.x, ray.y); */
+			/* printf("delta: %f %f\n", delta.x, delta.y); */
 			return (ray);
 		}
 		ray.x += delta.x;
@@ -78,50 +83,78 @@ void	raycast(t_world *world, t_player *playe)
 	t_vec	slope;
 	t_vec	hit;
 	t_vec	delta;
+	t_vec	delta2;
 	t_vec	hit2;
+	t_vec	 start;
+	t_vec	 start2;
 
 	t_vec	pos;
-	x = 0;
 	pos = playe->pos;
+	x = 0;
 	while (x < WIN_W)
 	{
-		 if (x == 960)
-			 x=961;
 		ray.x = playe->pos.x + playe->dir.x ;
 		ray.y = playe->pos.y + playe->dir.y ;
-		ray.x += playe->cam.x * ((x - WIN_W /2)/(double)WIN_W);
-		ray.y += playe->cam.y * ((x - WIN_W /2)/(double)WIN_W);
+		ray.x += playe->cam.x * ((x - WIN_W /2.0)/(double)WIN_W);
+		ray.y += playe->cam.y * ((x - WIN_W /2.0)/(double)WIN_W);
+		world->debug[project(ray.y)][project(ray.x)] = 0xff0000;
 		diff.x = ray.x - playe->pos.x;
 		diff.y = ray.y - playe->pos.y;
 		slope.x = fabs(diff.x / diff.y);
 		slope.y = fabs(diff.y / diff.x);
 		delta.x = slope.x * (diff.x / fabs(diff.x));
 		delta.y = diff.y / fabs(diff.y);
-		t_vec start;
-		start.x = 0;
-		hit = dda(world, ray, delta);
-		delta.x = diff.x / fabs(diff.x);
-		delta.y = slope.y * (diff.y / fabs(diff.y));
-		hit2 = dda(world, ray, delta);
-		if (slope.x <= slope.y)
+		start.y = floor(pos.y + delta.y);
+		if (delta.x < 0 && delta.y > 0)
+			slope.x = -slope.x;
+		else if (delta.x > 0 && delta.y < 0)
+			slope.x = -slope.x;
+		start.x = (start.y - pos.y) * slope.x  + pos.x;
+		hit.x = -1;
+		if ((start.x >= 0 && start.x < world->size.x - 1
+					&& start.y >= 0 && start.y < world->size.y  -1))
+		{
+			world->debug[project(start.y)][project(start.x)] = 0xFFFFFF;
+			hit = dda(world, start, delta);
+		}
+		delta2.x = diff.x / fabs(diff.x);
+		delta2.y = slope.y * (diff.y / fabs(diff.y));
+		start2.x = floor(pos.x + delta2.x);
+		if (delta2.x < 0 && delta2.y > 0)
+			slope.y = -slope.y;
+		else if (delta2.x > 0 && delta2.y < 0)
+			slope.y = -slope.y;
+		start2.y = (start2.x - pos.x) * slope.y + pos.y;
+		hit2.x = -1;
+		if ((start2.x >= 0 && start2.x < world->size.x - 1
+					&& start2.y >= 0 && start2.y < world->size.y  -1))
+		{
+			hit2 = dda(world, start2, delta2);
+			world->debug[project(start2.y)][project(start2.x)] = 0xFF00FF;
+		}
+		if (fabs(delta.x) > fabs(delta2.y))
 			color = 0xff;
 		else
 			color = 0xffff;
 		int len1;
 		int len2;
-		len1 = distance(hit, ray);
-		len2 = distance(hit2, ray);
-		if (len1 < len2)
+		len1 = distance(hit, pos);
+		len2 = distance(hit2, pos);
+		if (len1 < len2 )
 		{
-			draw_wall(world, playe, ray, hit, x, color);
-			plotline_vec(world->debug, (t_vec2){hit.x * (1080/60), hit.y * (1080/60)},
-					(t_vec2){ray.x * (1080/60), ray.y * (1080 /60) }, color);
+			if (hit.x != -1) {
+				draw_wall(world, playe, ray, hit, x, color);
+				/* plotline_vec(world->debug, (t_vec2){project(hit.x), project(hit.y) }, */
+				/* 		(t_vec2){project(ray.x), project(ray.y)}, color); */
+			}
 		}
 		else
 		{
-			draw_wall(world, playe, ray, hit2, x, color);
-			plotline_vec(world->debug, (t_vec2){hit2.x * (1080/60), hit2.y * (1080/60)},
-					(t_vec2){ray.x * (1080/60), ray.y * (1080 /60) }, color);
+			if (hit2.x != -1) {
+				draw_wall(world, playe, ray, hit2, x, color);
+				/* plotline_vec(world->debug, (t_vec2){project(hit2.x), project(hit2.y) }, */
+				/* 		(t_vec2){project(ray.x), project(ray.y)}, color); */
+			}
 		}
 		x++;
 	}
@@ -196,8 +229,8 @@ int			main(int	argc, char **argv)
 	data.world.map = init_world();
 	data.world.size = (t_vec){50, 50};
 	data.player.pos = (t_vec){25.1, 2.2};
-	data.player.dir = ((t_vec){0, 0.5});
-	data.player.cam = ((t_vec){0.5, 0});
+	data.player.dir = ((t_vec){0, 1});
+	data.player.cam = ((t_vec){1, 0});
 	data.mlx = mlx_init();
 	data.win = mlx_new_window(data.mlx, WIN_W, WIN_H, "wolf3d");
 	data.wind = mlx_new_window(data.mlx, WIN_W, WIN_H, "debug");
