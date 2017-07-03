475c518cbe5a0bd7bc80be048890e60910d365e4
Louis MENIGAULT <lmenigau@e3r1p12.42.fr>
2017-07-03 06:35:39 +0200

    ifej

--- wolf3d.c
+++ wolf3d.c
@@ -26,8 +26,8 @@ t_vec		dda(t_world *world, t_vec ray, t_vec delta)
 	map = (char (*)[50])world->map;
 	map[15][27] = 'X';
 	map[25][30] = 'X';
-	while (ray.x >= 0 && ray.x < world->size.x - 1
-			&& ray.y >= 0 && ray.y < world->size.y  -1)
+	while (ray.x >= 0 && ray.x < world->size.x
+			&& ray.y >= 0 && ray.y < world->size.y)
 	{
 		world->debug[project(ray.y)][project(ray.x)] = 0xF0FF33;
 		if(map[(int)(ray.y)][(int)(ray.x)] != ' ')
@@ -52,7 +52,7 @@ void	draw_wall(t_world *world, t_player *player, t_vec ray, t_vec hit, int col, 
 	int		y;
 
 	(void)player;
-	total.x =  fabs(hit.x - ray.x);
+	total.x = fabs(hit.x - ray.x);
 	total.y = fabs(hit.y - ray.y);
 	lenray = sqrtf(total.x * total.x + total.y * total.y);
 	//printf("%f, %f\n", hit.x, hit.y);
@@ -107,15 +107,14 @@ void	raycast(t_world *world, t_player *playe)
 		delta.x = slope.x * (diff.x / fabs(diff.x));
 		delta.y = diff.y / fabs(diff.y);
 		if ((delta.x < 0 && delta.y > 0)|| (delta.x > 0 && delta.y < 0))
-		{
 			slope.x = -slope.x;
+		if (delta.y < 0)
 			hdec = -1;
-		}
 		start.y = round(pos.y + hdec);
 		start.x = (start.y - pos.y) * slope.x  + pos.x;
 		hit.x = -1;
-		if ((start.x >= 0 && start.x < world->size.x - 1
-					&& start.y >= 0 && start.y < world->size.y  -1))
+		if ((start.x >= 0 && start.x < world->size.x
+					&& start.y >= 0 && start.y < world->size.y))
 		{
 			world->debug[project(start.y)][project(start.x)] = 0xFFFFFF;
 			hit = dda(world, start, delta);
@@ -123,15 +122,14 @@ void	raycast(t_world *world, t_player *playe)
 		delta2.x = diff.x / fabs(diff.x);
 		delta2.y = slope.y * (diff.y / fabs(diff.y));
 		if ((delta2.x < 0 && delta2.y > 0) || (delta2.x > 0 && delta2.y < 0))
-		{
 			slope.y = -slope.y;
+		if (delta2.x < 0)
 			vdec = -1;
-		}
 		start2.x = round(pos.x + vdec);
 		start2.y = (start2.x - pos.x) * slope.y + pos.y;
 		hit2.x = -1;
-		if ((start2.x >= 0 && start2.x < world->size.x - 1
-					&& start2.y >= 0 && start2.y < world->size.y  -1))
+		if ((start2.x >= 0 && start2.x < world->size.x
+					&& start2.y >= 0 && start2.y < world->size.y))
 		{
 			hit2 = dda(world, start2, delta2);
 			world->debug[project(start2.y)][project(start2.x)] = 0xFF00FF;
@@ -233,8 +231,8 @@ int			main(int	argc, char **argv)
 	data.world.map = init_world();
 	data.world.size = (t_vec){50, 50};
 	data.player.pos = (t_vec){25.1, 2.2};
-	data.player.dir = ((t_vec){0, 1});
-	data.player.cam = ((t_vec){1, 0});
+	data.player.dir = ((t_vec){0, 0.7});
+	data.player.cam = ((t_vec){0.7, 0});
 	data.mlx = mlx_init();
 	data.win = mlx_new_window(data.mlx, WIN_W, WIN_H, "wolf3d");
 	data.wind = mlx_new_window(data.mlx, WIN_W, WIN_H, "debug");
